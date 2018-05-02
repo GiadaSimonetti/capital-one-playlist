84693658efe6d5297ff6e53bf960c7de5e683909
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-05-02 12:19:27 +0100

    Update App.js and update default snapshot file

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 class App extends Component {
   render(){
     return (
-      <div />
+      <div>
+        <h1>The Playlist</h1>
+      </div>
     );
   }
 }
