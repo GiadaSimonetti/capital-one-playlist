14a641abf0525e332a4209a23d715d47597c4915
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-05-02 11:43:49 +0100

    Run npm i enzyme-adapter-react-16 --save-dev + add tempPolyfills.js and setupTests.js files to src folder

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
+import setupTests from './../setupTests.js';
+import tempPolyfills from './../tempPolyfills';
 import App from "./App";
 
 const app = shallow(<App />);
