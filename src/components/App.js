150a5496413697ff4925895ec41e301d75d764cb
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-05-02 14:04:53 +0100

    Add table with playlist

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,53 @@ class App extends Component {
     return (
       <div className="playlist">
         <h1>The Playlist</h1>
-        <div className="playlist-table"></div>
+        <div className="playlist-table">
+        <table>
+          <tr>
+            <th>Title</th>
+            <th>Artist</th>
+            <th>Album</th>
+            <th>Year</th>
+          </tr>
+          <tr>
+            <td>One More Time</td>
+            <td>Alison Moyet</td>
+            <td>The Turn</td>
+            <td>2007</td>
+          </tr>
+          <tr>
+            <td>Taurus II</td>
+            <td>Mike Oldfield</td>
+            <td>5 Miles Out</td>
+            <td>2013</td>
+          </tr>
+          <tr>
+            <td>F4</td>
+            <td>Spooky Tooth</td>
+            <td>The Island Years</td>
+            <td>2015</td>
+          </tr>
+          <tr>
+            <td>The Children Of Rarn</td>
+            <td>T-Rex</td>
+            <td>T-Rex</td>
+            <td>2014</td>
+          </tr>
+          <tr>
+            <td>Another Day</td>
+            <td>Alison Moyet</td>
+            <td>The Turn</td>
+            <td>2017</td>
+          </tr>
+          <tr>
+            <td>Face-off</td>
+            <td>Spooky Tooth</td>
+            <td>The Island Years</td>
+            <td>2015</td>
+          </tr>
+          </table>
+
+        </div>
       </div>
     );
   }
