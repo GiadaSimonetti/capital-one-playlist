9c009da886128fffee186c70b79931ee201ad168
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-05-03 10:02:44 +0100

    Update test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -18,12 +18,13 @@ describe("table", () =>{
   it("returns the number of `TableHeaderColumn`", () => {
     expect(app.find(".playlist-table").children().length).toEqual(4);
   });
-
-  it("checks the existence of `Title` column", () => {
-    expect(app.find(".title-field").exists()).toBe(true)
-  });
-
+  // 
   // it("checks the existence of `Title` column", () => {
   //   expect(app.find(".title-field").exists()).toBe(true)
   // });
+
+  it("checks the existence of `Title` column", () => {
+    expect(app.find(".title-field").props().children).toEqual("Title");
+  });
+
 });
